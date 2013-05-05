4bf762ab828138050c19a459da26d63ca0365ab0
Adrian Webb <adrian.webb@coraltech.net>
2013-05-04 20:34:27 -0400

    Initial commit with basic site.pp.

---
+++ site.pp
@@ -0,0 +1,60 @@
+/**
+ * Gateway manifest to all Puppet classes.
+ *
+ * Note: This is the only node in this system.  We use it to dynamically
+ * bootstrap or load and manage classes (profiles).
+ *
+ * This should allow the server to configure it's own profiles in the future.
+ */
+node default {
+
+  Exec {
+    logoutput => "on_failure",
+  }
+
+  #---
+
+  import "*.pp"
+  include coral::default
+
+  #---
+
+  resources { "firewall":
+    purge => true
+  }
+  Firewall {
+    before  => Class['coral::firewall::post_rules'],
+    require => Class['coral::firewall::pre_rules'],
+  }
+
+  include coral
+  include coral::firewall::pre_rules
+  include coral::firewall::post_rules
+
+  Class['coral::default'] -> Class['coral']
+
+  Exec {
+    user => $coral::params::exec_user,
+    path => $coral::params::exec_path
+  }
+
+  #---
+
+  import "profiles/*.pp"
+
+  if ! ( config_initialized and exists(global_param('config::common')) ) {
+    $config_address = global_param('config::address')
+
+    notice "Bootstrapping server"
+    notice "Push configurations to: ${config_address}"
+
+    include bootstrap
+    Class['coral'] -> Class['bootstrap']
+  }
+  else {
+    include base
+    Class['coral'] -> Class['base']
+
+    coral_include('profiles')
+  }
+}
