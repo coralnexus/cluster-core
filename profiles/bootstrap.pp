cc015ad3538acb6661f6d106484604be3442f931
Adrian Webb <adrian.webb@coraltech.net>
2013-05-12 22:20:30 -0400

    Fixing the cluster repository name in the bootstrap profile.

--- profiles/bootstrap.pp
+++ profiles/bootstrap.pp
@@ -8,7 +8,7 @@ class bootstrap {
 
   $cluster_source   = global_param('cluster_source')
   $cluster_revision = global_param('cluster_revision')
-  $cluster_repo_dir = global_param('cluster_repo_dir')
+  $cluster_repo     = global_param('cluster_repo')
 
   $post_update_commands = global_array('post_update_commands', [ $coral::params::puppet::update_command ])
 
@@ -28,7 +28,7 @@ class bootstrap {
   #-----------------------------------------------------------------------------
   # Resources
 
-  git::repo { $cluster_repo_dir:
+  git::repo { $cluster_repo:
     source               => $cluster_source,
     revision             => $cluster_revision,
     base                 => false,
