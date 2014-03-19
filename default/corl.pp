a7ca9d67172146d0f1ffa67994c3dd879716891e
Adrian Webb (local admin) <adrian.webb@coralnexus.com>
2014-03-19 04:42:38 -0400

    Updating quite a few CORL defaults to work more seamlessly with the updated CORL system.

--- default/corl.pp
+++ default/corl.pp
@@ -1,35 +1,40 @@
 
 class coralnexus::core::default::corl {
 
-  $puppet_dir = "${::corl_network}/build/puppetnode"
   $config_dir = "${::corl_network}/config"
-
+  
   #---
-
+  
   $puppet_config = {
-    main => {
-      modulepath  => join([ "${puppet_dir}/modules" ], ':'),
-      manifestdir => $puppet_dir,
-      manifest    => "${puppet_dir}/gateway.pp",
-      templatedir => "${puppet_dir}/templates"
+    'main' => {
+      'data_binding_terminus' => 'corl',
+      'report'                => true,
+      'reports'               => 'store',
+      'hiera_config'          => undef
     }
   }
+  
+  $puppet_update_command = "sudo corl provision"
+
+  #---
 
   $hiera_json_backend = {
     'type'    => 'json',
     'datadir' => $config_dir
   }
+  $hiera_yaml_backend = {
+    'type'    => 'yaml',
+    'datadir' => $config_dir
+  }
   $hiera_hierarchy = [
-    "identity/%{::server_identity}/%{::server_stage}",
-    "identity/%{::server_identity}",
-    "server/%{::hostname}/%{::server_stage}",
-    "server/%{::hostname}",
-    "location/%{::server_location}/%{::server_stage}",
-    "location/%{::server_location}",
-    "environment/%{::server_environment}/%{::server_stage}",
-    "environment/%{::server_environment}",
-    "stage/%{::server_stage}",
-    "type/%{::server_type}",
+    "identities/${::corl_identity}/stages/${::corl_stage}",
+    "identities/${::corl_identity}/identity",
+    "nodes/${::corl_provider}/${::fqdn}/stages/${::corl_stage}",
+    "nodes/${::corl_provider}/${::fqdn}/node",
+    "environments/${::corl_environment}/stages/${::corl_stage}",
+    "environments/${::corl_environment}/environment",
+    "stages/${::corl_stage}",
+    "types/${::corl_type}",
     "common"
   ]
 
@@ -38,7 +43,7 @@ class coralnexus::core::default::corl {
   $ssh_config = {
     'Port'                   => 22,
     'PermitRootLogin'        => 'yes',
-    'PermitEmptyPasswords'   => 'yes',
+    'PermitEmptyPasswords'   => 'no',
     'PasswordAuthentication' => 'yes',
     'AllowGroups'            => undef,
     'AllowUsers'             => ensure($::vagrant_exists, [ 'root', 'git', 'vagrant' ], [ 'root', 'git' ])
