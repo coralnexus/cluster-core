ee2e97e9150d5abcf2a2bb87a352969c9861146d
Adrian Webb <adrian.webb@coraltech.net>
2015-02-10 09:33:28 -0500

    Updating the php profile defaults.

--- default.pp
+++ default.pp
@@ -2,4 +2,15 @@
 class coralnexus::core::default {
   # Vagrant user MUST be "vagrant" right now due to the vagrant_exists fact checking for user name.
   $vagrant_user = 'vagrant'
+
+  $php_classes = [
+    "php::mod::intl",
+    "php::mod::apc",
+    "php::mod::curl",
+    "php::mod::gd",
+    "php::mod::imagick",
+    "php::mod::xmlrpc",
+    "php::mod::uploadprogress",
+    "php::mod::xdebug"
+  ]
 }
--- default/php.pp
+++
@@ -1,5 +0,0 @@
-
-class coralnexus::core::default::php {
-
-  $module_packages = [ 'php5-intl', 'php5-imagick' ]
-}
